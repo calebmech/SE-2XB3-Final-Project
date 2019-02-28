108666a78cf8ab84f1bcd603d0ef69f19bf748fd
Jonathan Janzen <janzej2@mcmaster.ca>
2019-02-28 15:15:13 -0500

    Added class for main method

---
+++ src/bixiTrip/BixiTrip.java
@@ -0,0 +1,11 @@
+package bixiTrip;
+
+public class BixiTrip {
+
+	public static void main(String args[]) {
+		// parse data
+		// create pastTrips
+		// and so on...
+	}
+
+}
--- src/bixiTrip/Main.java
+++
@@ -1,7 +0,0 @@
-package bixiTrip;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Running!");
-	}
-}
