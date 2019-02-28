e4c8056a70520b0b082e294664f56460fed7109b
Caleb Mech <cmech1@gmail.com>
2019-02-28 17:09:59 -0500

    Closes #6 - Initial implementation of Path class

--- src/bixiTrip/Path.java
+++ src/bixiTrip/Path.java
@@ -1,5 +1,23 @@
 package bixiTrip;
 
-public class Path {
+public class Path extends PastTrip {
+	private float duration;
+	private int count;
 
+	public Path(PastTrip trip) {
+		stationStart = trip.getStationStart();
+		stationEnd = trip.getStationEnd();
+		duration = trip.getDuration();
+		count = 1;
+	}
+
+	void addPastTrip(PastTrip trip) {
+		duration = (duration * count + trip.getDuration()) / (count + 1);
+		count++;
+	}
+
+	@Override
+	int getDuration() {
+		return Math.round(duration);
+	}
 }
