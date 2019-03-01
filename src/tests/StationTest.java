e396dd57cb1ae8cd982eb4644a11e2a4c6f546f6
Jonathan Janzen <janzej2@mcmaster.ca>
2019-03-01 16:03:04 -0500

    fixed formatting

--- src/tests/StationTest.java
+++ src/tests/StationTest.java
@@ -15,18 +15,18 @@ import bixiTrip.Coord;
 import bixiTrip.Station;
 
 public class StationTest {
-	
-	//create some test objects
-	Coord coord1 = new Coord(43.2609, 79.9192);
+
+	// create some test objects
+	Coord coord1 = new Coord(43.2609, -79.9192);
 	Coord coord2 = new Coord(-20.2984, 0.5748);
 	Station test1 = new Station(3056, "Station 1", coord1);
 	Station test2 = new Station(5049, "Station 2", coord2);
-	
+
 	@Test
 	public void testGetCode1() {
 		assert test1.getCode() == 3056;
 	}
-	
+
 	@Test
 	public void testGetCode2() {
 		assert test2.getCode() == 5049;
@@ -36,7 +36,7 @@ public class StationTest {
 	public void testGetName1() {
 		assert test1.getName().equals("Station 1");
 	}
-	
+
 	@Test
 	public void testGetName2() {
 		assert test2.getName().equals("Station 2");
@@ -46,7 +46,7 @@ public class StationTest {
 	public void testGetCoords1() {
 		assert test1.getCoords() == coord1;
 	}
-	
+
 	@Test
 	public void testGetCoords2() {
 		assert test2.getCoords() == coord2;
