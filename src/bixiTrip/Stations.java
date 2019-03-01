779cc642a2afeed120689bc8f810f19eafefe66c
Jonathan Janzen <janzej2@mcmaster.ca>
2019-03-01 14:34:36 -0500

    updates to functions
    created getStation function (not implemented yet)
    Added preliminary sorting implementation of addStation

--- src/bixiTrip/Stations.java
+++ src/bixiTrip/Stations.java
@@ -2,9 +2,10 @@ package bixiTrip;
 
 import java.util.ArrayList;
 
-/** 
- * A singleton datatype to store and perform operations
- * on a list of Stations.
+/**
+ * A singleton datatype to store and perform operations on a list of Stations.
+ * Singleton implementation based on information found at:
+ * https://www.geeksforgeeks.org/singleton-class-java/
  * 
  * @author Jonathan Janzen
  *
@@ -13,25 +14,60 @@ import java.util.ArrayList;
 public class Stations {
 
 	private static Stations single_instance = null;
-	
+
 	private ArrayList<Station> stations = new ArrayList<Station>();
-	
-	public static Stations Stations()
-	{
+
+	/**
+	 * Constructor for a singleton Stations() object.
+	 * 
+	 * @return Returns a single instance of the Stations object, whether one already
+	 *         exists or not.
+	 */
+	public static Stations Stations() {
 		if (single_instance == null)
 			single_instance = new Stations();
 		return single_instance;
 	}
 	
-	private void addStation(Station station) {
-		stations.add(station);
+	Station getStation(Integer code)
+	{
+		return null;
 	}
 	
-	private ArrayList<Station> getStations()
-	{
+	/**
+	 * Add a Station object to Stations.
+	 * 
+	 * @param station A Station object to be added to the Stations data type.
+	 */
+	void addStation(Station station) {
+		//add first station to object
+		if (stations.size() == 0) {
+			stations.add(station);
+		}
+		else {
+			//insert new station in sorted order
+			int i = 0;
+			while (i < stations.size()) {
+				if (stations.get(i).compareTo(station) > 0) {
+					stations.add(i, station);
+					break;
+				}
+				//increment i
+				i++;
+			}
+		}
+	}
+
+	/**
+	 * Function to return the stations stored in the instance of Stations.
+	 * 
+	 * @return Returns ArrayList<Stations> of stations stored.
+	 */
+	ArrayList<Station> getStations() {
 		return stations;
 	}
 	
+	//TEMPORARY TESTING FUNCTION
 	public static void main(String args[]) {
 		Stations stations = Stations.Stations();
 		Station test = new Station(4561, "Test Station 1", null);
