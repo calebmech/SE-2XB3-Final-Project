bad887cb257e160a8ef4809cc7824dcd009dcaf6
Harsh Mahajan <46697806+mahajan-harsh@users.noreply.github.com>
2019-03-03 20:03:22 -0500

    closes #16  Completed Documentation for Coord

--- src/bixiTrip/Coord.java
+++ src/bixiTrip/Coord.java
@@ -1,22 +1,35 @@
 package bixiTrip;
 /**
+ * This class initializes the co-ordinates of the stations.
  * 
- * @author hdmah
+ * @author Harsh Mahajan
  *
  */
 public class Coord {
 	private final double longitude;
 	private final double latitude;
-
+	
+	/**
+	 * Constructor that assigns the longitude and latitude to the respective station object.
+	 * 
+	 * @param _longitude 
+	 * @param _latitude
+	 */
 	public Coord(double _longitude, double _latitude) {
 		this.longitude = _longitude;
 		this.latitude = _latitude;
 	}
-
+	/**
+	 * Accessor function for the Longitude.       
+	 * @return
+	 */
 	public double getLongitude() {
 		return longitude;
 	}
-
+	/**
+	 * Accessor function for the Latitude.       
+	 * @return
+	 */
 	public double getLatitude() {
 		return latitude;
 	}
