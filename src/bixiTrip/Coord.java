291d9fb9b8330767edea5254959f6cc7a17bd5d0
Jonathan Janzen <janzej2@mcmaster.ca>
2019-03-04 14:19:59 -0500

    formatted coord class

--- src/bixiTrip/Coord.java
+++ src/bixiTrip/Coord.java
@@ -1,4 +1,5 @@
 package bixiTrip;
+
 /**
  * This class initializes the co-ordinates of the stations.
  * 
@@ -8,26 +9,31 @@ package bixiTrip;
 public class Coord {
 	private final double longitude;
 	private final double latitude;
-	
+
 	/**
-	 * Constructor that assigns the longitude and latitude to the respective station object.
+	 * Constructor that assigns the longitude and latitude to the respective station
+	 * object.
 	 * 
-	 * @param _longitude 
+	 * @param _longitude
 	 * @param _latitude
 	 */
 	public Coord(double _longitude, double _latitude) {
 		this.longitude = _longitude;
 		this.latitude = _latitude;
 	}
+
 	/**
-	 * Accessor function for the Longitude.       
+	 * Accessor function for the Longitude.
+	 * 
 	 * @return
 	 */
 	public double getLongitude() {
 		return longitude;
 	}
+
 	/**
-	 * Accessor function for the Latitude.       
+	 * Accessor function for the Latitude.
+	 * 
 	 * @return
 	 */
 	public double getLatitude() {
