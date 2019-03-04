29b07559071ca9f299f5bec7b96c274993d287b3
Harsh Mahajan <46697806+mahajan-harsh@users.noreply.github.com>
2019-03-03 19:58:05 -0500

    closes #5  Implemented Co-ordinate Class

--- src/bixiTrip/Coord.java
+++ src/bixiTrip/Coord.java
@@ -1,5 +1,23 @@
 package bixiTrip;
-
+/**
+ * 
+ * @author hdmah
+ *
+ */
 public class Coord {
+	private final double longitude;
+	private final double latitude;
 
+	public Coord(double _longitude, double _latitude) {
+		this.longitude = _longitude;
+		this.latitude = _latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
 }
