819f407aa0af8b5ae8da9f6d5e59fe4130ea4571
matthewbraden <bradenm@mcmaster.ca>
2019-03-05 16:21:00 -0500

    Starting on the PastTrips class

--- src/bixiTrip/PastTrips.java
+++ src/bixiTrip/PastTrips.java
@@ -1,5 +1,44 @@
 package bixiTrip;
 
-public class PastTrips {
+import java.util.ArrayList;
 
+/**
+ * Class that contains the access to different trips
+ * 
+ * @author matthewbraden
+ *
+ */
+public class PastTrips {
+	
+	private static PastTrips instance = null;
+	private boolean isSorted = true;
+	private ArrayList<PastTrip> pasttrips = new ArrayList<PastTrip>();
+	
+	/**
+	 * Function that gets a list of past trips
+	 */
+	public getInstance() {
+		//this.trip = ;
+	}
+	
+	/**
+	 * 
+	 * Function that returns a list of past trips between two stations
+	 * 
+	 * @param start The starting station of the trip
+	 * @param end The end station of the trip
+	 * @return List of past trips
+	 */
+	public PastTrips[] getTrips(Station start, Station end) {
+		
+	}
+	
+	/**
+	 * Function that adds a trip to the list
+	 * 
+	 * @param trip A new single trip
+	 */
+	public void addTrip(PastTrip trip) {
+		
+	}
 }
