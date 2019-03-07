e3f55956b4912f9feee5a25a6e26f69e4dbd838e
Jonathan Janzen <janzej2@mcmaster.ca>
2019-03-07 15:19:43 -0500

    formatted code

--- src/algs/BinarySearch.java
+++ src/algs/BinarySearch.java
@@ -16,13 +16,14 @@ import java.util.Arrays;
 import bixiTrip.Station;
 
 public class BinarySearch {
-	
-	/** 
+
+	/**
 	 * Implementation of binary search using code from Sedgewick & Wayne's
-	 * "Algorithms: Fourth Edition" on page 9. Minor modifications have been
-	 * made to implement searching on Stations based on an integer, in order
-	 * to match the API.
-	 * @param a An arraylist of type Station to be searched.
+	 * "Algorithms: Fourth Edition" on page 9. Minor modifications have been made to
+	 * implement searching on Stations based on an integer, in order to match the
+	 * API.
+	 * 
+	 * @param a   An arraylist of type Station to be searched.
 	 * @param key An integer code to search for
 	 * @return
 	 */
@@ -31,9 +32,12 @@ public class BinarySearch {
 		int hi = a.size() - 1;
 		while (lo <= hi) {
 			int mid = lo + (hi - lo) / 2;
-			if (code < a.get(mid).getCode()) hi = mid - 1;
-			else if (code > a.get(mid).getCode()) lo = mid + 1;
-			else return mid;
+			if (code < a.get(mid).getCode())
+				hi = mid - 1;
+			else if (code > a.get(mid).getCode())
+				lo = mid + 1;
+			else
+				return mid;
 		}
 		return -1;
 	}
