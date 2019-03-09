5ea910f49ad8fd4a7cc102cbfa94525c534c568b
Jonathan Janzen <janzej2@mcmaster.ca>
2019-03-09 17:12:49 -0500

    made fake functions for implementing main()

--- src/bixiTrip/Parser.java
+++ src/bixiTrip/Parser.java
@@ -1,5 +1,19 @@
 package bixiTrip;
 
 public class Parser {
-
+	
+	
+	PastTrips getPastTrips(String dirPath) {
+		//temporary for writing the main function
+		//assume that a PastTrips object has been created already
+		PastTrips fake = new PastTrips();
+		return fake;
+	}
+	
+	Stations getStations(String filePath) {
+		//temporary for writing the main function
+		//assume that a Stations object has been created already
+		Stations fake = new Stations();
+		return fake;
+	}
 }
