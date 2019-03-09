2aeaea9523e04215d8c94958cff6fcdd78161f47
Jonathan Janzen <janzej2@mcmaster.ca>
2019-03-09 17:58:30 -0500

    updated fake functions to use getInstance()

--- src/bixiTrip/Parser.java
+++ src/bixiTrip/Parser.java
@@ -3,15 +3,17 @@ package bixiTrip;
 public class Parser {
 	
 	
-	public PastTrips parsePastTrips(String dirPath, PastTrips pastTrips) {
+	public PastTrips parsePastTrips(String dirPath) {
 		//temporary for writing the main function
 		//assume that a PastTrips object has been created already
+		PastTrips pastTrips = PastTrips.getInstance();
 		return pastTrips;
 	}
 	
-	public Stations parseStations(String filePath, Stations stations) {
+	public Stations parseStations(String filePath) {
 		//temporary for writing the main function
 		//assume that a Stations object has been created already
+		Stations stations = Stations.getInstance();
 		return stations;
 	}
 }
