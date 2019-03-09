4e4c784aba1b0210f59aad635548fc0d34544b8a
Harsh Mahajan <46697806+mahajan-harsh@users.noreply.github.com>
2019-03-08 23:59:56 -0500

    closes #19  testCoord

---
+++ src/tests/testCoord.java
@@ -0,0 +1,31 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import bixiTrip.Coord;
+import bixiTrip.Station;
+
+public class testCoord {
+	// create some test objects
+		Coord coord1 = new Coord(43.2609, -79.9192);
+		Coord coord2 = new Coord(-20.2984, 0.5748);
+		Station test1 = new Station(3056, "Station 1", coord1);
+		Station test2 = new Station(5049, "Station 2", coord2);
+	
+
+	@Test
+	public void testLogitude() {
+		assert coord1.getLongitude() == 43.26069;
+		assert coord2.getLongitude() == -20.2984;
+	}
+	
+	@Test
+	public void testLatitude() {
+		assert coord1.getLatitude() == -79.9192;
+		assert coord2.getLatitude() == 0.5748;
+	}
+}
