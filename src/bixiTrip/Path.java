2c7eeed38598b246ed2ff6d23d5d55caf34ac5f4
Caleb Mech <cmech1@gmail.com>
2019-03-10 21:58:56 -0400

    Updated to match v1.1 of API

--- src/bixiTrip/Path.java
+++ src/bixiTrip/Path.java
@@ -3,16 +3,26 @@ package bixiTrip;
 public class Path extends PastTrip {
 	private float duration;
 	private int count;
+	private Stations stations;
 
 	public Path(PastTrip _trip) {
 		super(_trip.getStationStart(), _trip.getStationEnd(), 0);
 		this.duration = _trip.getDuration();
+		this.stations = Stations.getInstance();
 		count = 1;
 	}
 
 	public void addPastTrip(PastTrip trip) {
 		duration = (duration * count + trip.getDuration()) / (count + 1);
 		count++;
+	}
+	
+	public int getStartIndex() {
+		return stations.getIndex(stationStart);
+	}
+	
+	public int getEndIndex() {
+		return stations.getIndex(stationEnd);
 	}
 
 	@Override
