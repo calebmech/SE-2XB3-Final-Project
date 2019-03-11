404bab90db66bc57f93cecbe91706f2e12bf1bdf
Caleb Mech <cmech1@gmail.com>
2019-03-11 12:56:51 -0400

    Documented Trip class - Closes #18

--- src/bixiTrip/Trip.java
+++ src/bixiTrip/Trip.java
@@ -2,18 +2,37 @@ package bixiTrip;
 
 import algs.Queue;
 
+/**
+ * Trip abstract data type that provides directions
+ * between two Stations
+ * 
+ * @author Caleb Mech &amp; Jonathan Janzen
+ *
+ */
+
 public class Trip {
 	private Station start;
 	private Station end;
 	private Queue<Station> route;
 	private Boolean hasRoute = false;
 
+	/**
+	 * Constructor for Trip object
+	 * 
+	 * @param _start 	Code of Station to start from
+	 * @param _end		Code of Station to end at
+	 */
 	public Trip(Station _start, Station _end) {
 		this.start = _start;
 		this.end = _end;
 		this.route = new Queue<Station>();
 	}
 
+	/**
+	 * Get route between two Stations
+	 * 
+	 * @return Returns sequence of Stations to travel
+	 */
 	public Iterable<Station> getRoute() {
 		if (hasRoute == false) {
 			Paths paths = Paths.getInstance();
@@ -29,6 +48,11 @@ public class Trip {
 		return route;
 	}
 	
+	/**
+	 * Get URL for directions to travel a route
+	 * 
+	 * @return Returns string representation of directions URL
+	 */
 	public String getUrl() {
 		if (!hasRoute)
 			getRoute();
