dcb4cb9cc13b9cd8e5efde80b420eddb725c12c2
Jonathan Janzen <janzej2@mcmaster.ca>
2019-03-10 22:23:21 -0400

    Updated Stations and StationsTest to API v1.1

--- src/tests/StationsTest.java
+++ src/tests/StationsTest.java
@@ -1,4 +1,5 @@
 package tests;
+
 /** 
  * Testing class for the Stations object.
  * 
@@ -18,12 +19,12 @@ import org.junit.After;
 import org.junit.Before;
 
 public class StationsTest {
-	
+
 	Stations stations = new Stations();
-	
+
 	@Before
 	public void setUp() throws Exception {
-		//setup a new Stations object to do tests on
+		// setup a new Stations object to do tests on
 		stations = new Stations();
 		Station station1 = new Station(43, "Station 1", new Coord(4.5843, 9.2940));
 		Station station2 = new Station(33, "Station 2", new Coord(69.5435, -12.2423));
@@ -32,33 +33,63 @@ public class StationsTest {
 		stations.addStation(station2);
 		stations.addStation(station3);
 	}
-	
-	//case1 where station is present
+
+	// case1 where station is present
 	@Test
 	public void testGetStation1() {
-		assert stations.getStation(43).getCode() == 43;
+		assert stations.getStationByCode(43).getCode() == 43;
 	}
-	
-	//case2 where station is present
+
+	// case2 where station is present
 	@Test
 	public void testGetStation2() {
-		assert stations.getStation(484).getCode() == 484;
+		assert stations.getStationByCode(484).getCode() == 484;
 	}
-	
-	//case3 where station is not present
+
+	// case3 where station is not present
 	@Test(expected = ArrayIndexOutOfBoundsException.class)
 	public void testGetStation3() {
-		stations.getStation(11);
+		stations.getStationByCode(11);
 	}
-	
-	//general test case to ensure stations are returned properly
+
+	// general test case to ensure stations are returned properly
 	@Test
 	public void testGetStations() {
 		ArrayList<Station> out = stations.getStations();
-		Integer[] expected = {33, 43, 484};
+		Integer[] expected = { 33, 43, 484 };
 		for (int i = 0; i < out.size(); i++) {
 			assert (out.get(i).getCode() - expected[i] == 0);
 		}
+	}
+
+	// general test 1 for getIndex
+	@Test
+	public void testGetStationByIndex1() {
+		assert stations.getStationByIndex(1).getCode() == 43;
+	}
+
+	// general test 2 for getIndex
+	@Test
+	public void testGetStationByIndex2() {
+		assert stations.getStationByIndex(2).getCode() == 484;
+	}
+
+	// case where index value does not exist
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testGetStationByIndex3() {
+		stations.getStationByIndex(3);
+	}
+
+	// general test 1 for getIndex
+	@Test
+	public void testGetIndex1() {
+		assert stations.getIndex(33) == 0;
+	}
+
+	// case where index value does not exist
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testGetIndex2() {
+		stations.getIndex(-1);
 	}
 
 }
