89e2e60e3c4240aed71e55ac0ef19e222860a163
Caleb Mech <cmech1@gmail.com>
2019-03-10 22:02:02 -0400

    Updated to use edge weighted graph - Closes #28

--- src/algs/Graph.java
+++ src/algs/Graph.java
@@ -1,16 +1,18 @@
 package algs;
 
+import bixiTrip.Path;
+
 public class Graph {
 	private final int V;
 	private int E;
-	private Bag<Integer>[] adj;
+	private Bag<Path>[] adj;
 
 	public Graph(int _V) {
 		this.V = _V;
 		this.E = 0;
-		adj = (Bag<Integer>[]) new Bag[V];
+		adj = (Bag<Path>[]) new Bag[V];
 		for (int v = 0; v < V; v++)
-			adj[v] = new Bag<Integer>();
+			adj[v] = new Bag<Path>();
 	}
 
 	public int V() {
@@ -21,12 +23,29 @@ public class Graph {
 		return E;
 	}
 
-	public void addEdge(int v, int w) {
-		adj[v].add(w);
+	public void addPath(Path p) {
+		adj[p.getStartIndex()].add(p);
 		E++;
 	}
 	
-	public Iterable<Integer> adj(int v) {
+	public Path getPath(int startIndex, int endIndex) {
+		for (Path p : adj[startIndex]) {
+			if (p.getEndIndex() == endIndex) {
+				return p;
+			}
+		}
+		return null;
+	}
+	
+	public Iterable<Path> adj(int v) {
 		return adj[v];
+	}
+	
+	public Iterable<Path> edges() {
+		Bag<Path> bag = new Bag<Path>();
+		for (int v = 0; v < V; v++)
+			for (Path p : adj[v])
+				bag.add(p);
+		return bag;
 	}
 }
