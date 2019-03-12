fe1a6f806f603f4c395e77c96a77a8aa6b5b08d9
Caleb Mech <cmech1@gmail.com>
2019-03-11 23:48:30 -0400

    Throw exception if stations are not connected

--- src/bixiTrip/Trip.java
+++ src/bixiTrip/Trip.java
@@ -36,8 +36,11 @@ public class Trip {
 		this.stations = Stations.getInstance();
 		this.sp = new SP(graph, start.getCode());
 
+		if (!sp.hasPathTo(end.getCode()))
+			throw new IllegalArgumentException("Stations are not connected.");
+		
 		Stack<Path> pathSeq = sp.pathTo(end.getCode());
-
+		
 		int i = pathSeq.size();
 		for (Path path : pathSeq) {
 			route.enqueue(stations.getStationByIndex(path.getStartIndex()));
