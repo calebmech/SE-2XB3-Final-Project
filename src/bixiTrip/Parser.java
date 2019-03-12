08fd894569a9ba5db6678ba83d3ad91f54ee7b78
Caleb Mech <cmech1@gmail.com>
2019-03-11 23:11:20 -0400

    Close input files when done with them

--- src/bixiTrip/Parser.java
+++ src/bixiTrip/Parser.java
@@ -16,44 +16,47 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Parser {
-	
-	
+
 	/**
 	 * Parse CSV of past trips information
+	 * 
 	 * @param dirPath inputted directory path
 	 * @return object containing past trips data
 	 */
 	public static PastTrips parsePastTrips(String dirPath) {
-		
+
 		// Get instance/any previous instance of pastTrips object
 		PastTrips pastTrips = PastTrips.getInstance();
-		
+
 		// List of files within provided directory path
 		File folder = new File(dirPath);
 		File[] listOfFiles = folder.listFiles();
-		
+
 		for (File file : listOfFiles) {
 			if (file.isFile()) {
 				try {
 					Scanner input = new Scanner(file);
-					input.nextLine();
-					
+					if (input.hasNextLine())
+						input.nextLine();
+
 					// Read each line of the file and split by comma regex
 					while (input.hasNext()) {
 						String trip = input.nextLine();
 						String[] tempArray = trip.split(",");
-						
+
 						// Local variables to store attributes of each trip
 						int startStation = Integer.parseInt(tempArray[1]);
 						int endStation = Integer.parseInt(tempArray[3]);
 						int duration = Integer.parseInt(tempArray[4]);
-						
+
 						// Add a past trip to past trips object
-						pastTrips.addTrip(new PastTrip(startStation, endStation, duration));
-						
+						pastTrips.addTrip(new PastTrip(startStation, endStation,
+								duration));
+
 					}
+					input.close();
 				}
-				
+
 				// Catch block
 				catch (FileNotFoundException e) {
 					e.printStackTrace();
@@ -63,9 +66,10 @@ public class Parser {
 
 		return pastTrips;
 	}
-	
+
 	/**
 	 * Parse CSV of station information
+	 * 
 	 * @param filePath inputted file path
 	 * @return object containing station data
 	 */
@@ -73,32 +77,33 @@ public class Parser {
 
 		// Get instance/any previous instance of stations object
 		Stations stations = Stations.getInstance();
-		
+
 		File file = new File(filePath);
-		
+
 		try {
 			Scanner input = new Scanner(file);
 			input.nextLine();
-			
+
 			// Read each line of the file and split by comma regex
 			while (input.hasNext()) {
 				String station = input.nextLine();
 				String[] tempArray1 = station.split(",");
-				
+
 				// Local variables to store attributes of each station
 				int code = Integer.parseInt(tempArray1[0]);
-				
+
 				String name = tempArray1[1];
-				
+
 				double latitude = Double.parseDouble(tempArray1[2]);
 				double longitude = Double.parseDouble(tempArray1[3]);
 				Coord coords = new Coord(latitude, longitude);
-				
+
 				// Add a station to stations object
 				stations.addStation(new Station(code, name, coords));
 			}
+			input.close();
 		}
-		
+
 		// Catch block
 		catch (FileNotFoundException e) {
 			e.printStackTrace();
