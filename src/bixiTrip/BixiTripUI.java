f220a92a8bcec2d2dcba7d47056c4d7e26235484
Jonathan Janzen <janzej2@mcmaster.ca>
2019-03-16 12:54:23 -0400

    Backend up to trip parsing implemented, mostly. issue with UI freezing when parsing

--- src/bixiTrip/BixiTripUI.java
+++ src/bixiTrip/BixiTripUI.java
@@ -18,12 +18,15 @@ import javax.swing.JComboBox;
 import javax.swing.JTextField;
 import javax.swing.JProgressBar;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 
 public class BixiTripUI extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField statusField;
+	private static JTextField statusField;
 
 	/**
 	 * Launch the application.
@@ -40,7 +43,7 @@ public class BixiTripUI extends JFrame {
 			}
 		});
 	}
-
+	
 	/**
 	 * Create the frame.
 	 */
@@ -61,17 +64,8 @@ public class BixiTripUI extends JFrame {
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 32));
 		contentPane.add(lblNewLabel);
 		
-		JButton directionsButton = new JButton("Get Directions");
-		directionsButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		directionsButton.setEnabled(false);
-		directionsButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		directionsButton.setBounds(199, 166, 200, 38);
-		contentPane.add(directionsButton);
-		
 		JComboBox startStationComboBox = new JComboBox();
+		startStationComboBox.setEnabled(false);
 		startStationComboBox.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		startStationComboBox.setBounds(176, 53, 404, 38);
 		contentPane.add(startStationComboBox);
@@ -87,20 +81,91 @@ public class BixiTripUI extends JFrame {
 		contentPane.add(lblEndStation);
 		
 		JComboBox endStationComboBox = new JComboBox();
+		endStationComboBox.setEnabled(false);
 		endStationComboBox.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		endStationComboBox.setBounds(176, 115, 404, 38);
 		contentPane.add(endStationComboBox);
 		
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setBounds(12, 217, 568, 14);
+		contentPane.add(progressBar);
+		
+		JButton directionsButton = new JButton("Get Directions");
+		directionsButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String pastTripsPath;
+				File pastTripsDir;
+				PastTrips pastTrips;
+				Station start, end;
+				Integer startCode, endCode;
+				Trip mainTrip;
+				
+				pastTrips = new PastTrips();
+				pastTrips = pastTrips.getInstance();
+				
+				if (startStationComboBox.getSelectedItem() == "" || endStationComboBox.getSelectedItem() == "") {
+					statusField.setForeground(new Color(255, 0, 0));
+					statusField.setText("One or more selections is incorrect. Please select a start and end station.");
+					return;
+				}
+				statusField.setForeground(new Color(0, 0, 0));
+				statusField.setText("Parsing past trips...");
+				progressBar.setIndeterminate(true);
+				
+				pastTripsPath = "pastTrips";
+				pastTripsDir = new File(pastTripsPath);
+				try {
+					pastTrips = Parser.parsePastTrips(pastTripsPath);
+				} catch (Exception f) {
+					statusField.setForeground(new Color(255, 0, 0));
+					statusField.setText("Past trips directory couldn't be found. Please correct the file path and try again.");
+					return;
+				}
+				//progressBar.setIndeterminate(false);
+			}
+		});
+		directionsButton.setEnabled(false);
+		directionsButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		directionsButton.setBounds(199, 166, 200, 38);
+		contentPane.add(directionsButton);
+		
 		statusField = new JTextField();
 		statusField.setEditable(false);
-		statusField.setHorizontalAlignment(SwingConstants.LEFT);
-		statusField.setFont(new Font("Tahoma", Font.ITALIC, 16));
+		statusField.setHorizontalAlignment(SwingConstants.CENTER);
+		statusField.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		statusField.setBounds(12, 244, 568, 38);
 		contentPane.add(statusField);
 		statusField.setColumns(10);
 		
-		JProgressBar progressBar = new JProgressBar();
-		progressBar.setBounds(12, 217, 568, 14);
-		contentPane.add(progressBar);
+		//start general main implementation
+		String url, stationPath = "stations\\stations_2018.csv", pastTripsPath = "pastTrips";
+		Paths paths;
+		File stationFile, pastTripsDir;
+		
+		// initialize PastTrips and Stations abstract objects
+		Stations stations = new Stations();
+		PastTrips pastTrips = new PastTrips();
+		
+		stationFile = new File(stationPath);
+		if (!stationFile.exists()) {
+			statusField.setForeground(new Color(255, 0, 0));
+			statusField.setText("Stations file could not be found. Please correct the file path and try again.");
+			return;
+		}
+		stations = Parser.parseStations(stationPath);
+		statusField.setText("Welcome to BixiTrip! Please select your starting and ending locations.");
+		
+		//populate combobox
+		ArrayList<Station> stationsList = stations.getStations();
+		startStationComboBox.setEnabled(true);
+		endStationComboBox.setEnabled(true);
+		
+		startStationComboBox.addItem("");
+		endStationComboBox.addItem("");
+		for (Station element : stationsList) {
+			startStationComboBox.addItem(element.getName());
+			endStationComboBox.addItem(element.getName());
+		}
+		directionsButton.setEnabled(true);
 	}
 }
