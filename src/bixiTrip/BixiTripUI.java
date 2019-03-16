b97fe826b5404c9024cca96828c32029cde09db6
Jonathan Janzen <janzej2@mcmaster.ca>
2019-03-16 13:53:52 -0400

    implemented background tasks using SwingWorker. no longer freezes on parsing. closes #49

--- src/bixiTrip/BixiTripUI.java
+++ src/bixiTrip/BixiTripUI.java
@@ -1,5 +1,12 @@
 package bixiTrip;
 
+/** 
+ * The main UI for BixiTrip. Likely requires WindowBuilder for Eclipse to be installed
+ * in order to function properly.
+ * 
+ * @author Jonathan Janzen
+ */
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
@@ -9,6 +16,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import javax.swing.SwingWorker;
+
 import java.awt.Color;
 import javax.swing.border.BevelBorder;
 import java.awt.SystemColor;
@@ -26,8 +35,10 @@ import java.awt.event.ActionEvent;
 public class BixiTripUI extends JFrame {
 
 	private JPanel contentPane;
-	private static JTextField statusField;
-
+	private JTextField statusField = new JTextField();
+	private Boolean isParsed = false;
+	private JProgressBar progressBar = new JProgressBar();
+	private JButton directionsButton = new JButton("Get Directions");
 	/**
 	 * Launch the application.
 	 */
@@ -43,11 +54,12 @@ public class BixiTripUI extends JFrame {
 			}
 		});
 	}
-	
+
 	/**
 	 * Create the frame.
 	 */
 	public BixiTripUI() {
+
 		setTitle("BixiTrip");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 610, 339);
@@ -56,41 +68,41 @@ public class BixiTripUI extends JFrame {
 		contentPane.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("BixiTrip");
 		lblNewLabel.setForeground(new Color(255, 0, 0));
 		lblNewLabel.setBounds(2, 2, 590, 38);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 32));
 		contentPane.add(lblNewLabel);
-		
+
 		JComboBox startStationComboBox = new JComboBox();
 		startStationComboBox.setEnabled(false);
 		startStationComboBox.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		startStationComboBox.setBounds(176, 53, 404, 38);
 		contentPane.add(startStationComboBox);
-		
+
 		JLabel lblStartStation = new JLabel("Start Station:");
 		lblStartStation.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		lblStartStation.setBounds(22, 54, 142, 37);
 		contentPane.add(lblStartStation);
-		
+
 		JLabel lblEndStation = new JLabel("End Station:");
 		lblEndStation.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		lblEndStation.setBounds(22, 115, 142, 37);
 		contentPane.add(lblEndStation);
-		
+
 		JComboBox endStationComboBox = new JComboBox();
 		endStationComboBox.setEnabled(false);
 		endStationComboBox.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		endStationComboBox.setBounds(176, 115, 404, 38);
 		contentPane.add(endStationComboBox);
-		
-		JProgressBar progressBar = new JProgressBar();
+
+		//JProgressBar progressBar = new JProgressBar();
 		progressBar.setBounds(12, 217, 568, 14);
 		contentPane.add(progressBar);
-		
-		JButton directionsButton = new JButton("Get Directions");
+
+		//JButton directionsButton = new JButton("Get Directions");
 		directionsButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String pastTripsPath;
@@ -99,53 +111,39 @@ public class BixiTripUI extends JFrame {
 				Station start, end;
 				Integer startCode, endCode;
 				Trip mainTrip;
-				
+
 				pastTrips = new PastTrips();
 				pastTrips = pastTrips.getInstance();
-				
+
 				if (startStationComboBox.getSelectedItem() == "" || endStationComboBox.getSelectedItem() == "") {
 					statusField.setForeground(new Color(255, 0, 0));
 					statusField.setText("One or more selections is incorrect. Please select a start and end station.");
 					return;
 				}
-				statusField.setForeground(new Color(0, 0, 0));
-				statusField.setText("Parsing past trips...");
-				progressBar.setIndeterminate(true);
-				
-				pastTripsPath = "pastTrips";
-				pastTripsDir = new File(pastTripsPath);
-				try {
-					pastTrips = Parser.parsePastTrips(pastTripsPath);
-				} catch (Exception f) {
-					statusField.setForeground(new Color(255, 0, 0));
-					statusField.setText("Past trips directory couldn't be found. Please correct the file path and try again.");
-					return;
-				}
-				//progressBar.setIndeterminate(false);
 			}
 		});
 		directionsButton.setEnabled(false);
 		directionsButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		directionsButton.setBounds(199, 166, 200, 38);
 		contentPane.add(directionsButton);
-		
-		statusField = new JTextField();
+
+		//statusField = new JTextField();
 		statusField.setEditable(false);
 		statusField.setHorizontalAlignment(SwingConstants.CENTER);
 		statusField.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		statusField.setBounds(12, 244, 568, 38);
 		contentPane.add(statusField);
 		statusField.setColumns(10);
-		
-		//start general main implementation
+
+		// start general main implementation
 		String url, stationPath = "stations\\stations_2018.csv", pastTripsPath = "pastTrips";
 		Paths paths;
 		File stationFile, pastTripsDir;
-		
+
 		// initialize PastTrips and Stations abstract objects
 		Stations stations = new Stations();
 		PastTrips pastTrips = new PastTrips();
-		
+
 		stationFile = new File(stationPath);
 		if (!stationFile.exists()) {
 			statusField.setForeground(new Color(255, 0, 0));
@@ -153,19 +151,52 @@ public class BixiTripUI extends JFrame {
 			return;
 		}
 		stations = Parser.parseStations(stationPath);
-		statusField.setText("Welcome to BixiTrip! Please select your starting and ending locations.");
-		
-		//populate combobox
+		statusField.setText("Welcome to BixiTrip! Please wait while we import our data.");
+
+		// populate combobox
 		ArrayList<Station> stationsList = stations.getStations();
-		startStationComboBox.setEnabled(true);
-		endStationComboBox.setEnabled(true);
-		
+
 		startStationComboBox.addItem("");
 		endStationComboBox.addItem("");
 		for (Station element : stationsList) {
 			startStationComboBox.addItem(element.getName());
 			endStationComboBox.addItem(element.getName());
 		}
-		directionsButton.setEnabled(true);
+		startStationComboBox.setEnabled(true);
+		endStationComboBox.setEnabled(true);
+
+		// parse data in background
+		progressBar.setIndeterminate(true);
+		parse();
+	}
+
+	public void parse() {
+		SwingWorker<Boolean, String> worker = new SwingWorker<Boolean, String>() {
+			// @Override
+			protected Boolean doInBackground() throws Exception {
+				String pastTripsPath;
+				File pastTripsDir;
+				PastTrips pastTrips = new PastTrips();
+				pastTrips = pastTrips.getInstance();
+
+				pastTripsPath = "pastTrips";
+				pastTripsDir = new File(pastTripsPath);
+				try {
+					pastTrips = Parser.parsePastTrips(pastTripsPath);
+					isParsed = true;
+				} catch (Exception f) {
+					statusField.setForeground(new Color(255, 0, 0));
+					statusField.setText(
+							"Past trips directory couldn't be found. Please correct the file path and try again.");
+					return false;
+				}
+				progressBar.setIndeterminate(false);
+				directionsButton.setEnabled(true);
+				statusField.setForeground(new Color(0, 0, 0));
+				statusField.setText("Imported successfully. Direction finding enabled.");
+				return true;
+			}
+		};
+		worker.execute();
 	}
 }
