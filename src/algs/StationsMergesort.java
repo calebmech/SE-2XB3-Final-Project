9ee31d0cfa412a88a016b211a14b23acb8b9add8
Jonathan Janzen <janzej2@mcmaster.ca>
2019-03-18 14:36:56 -0400

    Added mergesort by name for Stations

--- src/algs/StationsMergesort.java
+++ src/algs/StationsMergesort.java
@@ -15,7 +15,7 @@ import bixiTrip.Station;
 
 public class StationsMergesort {
 
-	private static Station[] aux;
+	private static Station[] aux, aux2;
 
 	/**
 	 * Code for less taken from page 245 of "Algorithms: Fourth Edition" by
@@ -67,6 +67,52 @@ public class StationsMergesort {
 		for (int len = 1; len < n; len *= 2)
 			for (int lo = 0; lo < n - len; lo += len + len)
 				merge(a, lo, lo + len - 1, Math.min(lo + len + len - 1, n - 1));
+	}
+
+	/**
+	 * Function to merge. Code based on Sedgewick & Wayne's "Algorithms: Fourth
+	 * Edition" on page 271, but modified to implement the Station type and sort by
+	 * name.
+	 * 
+	 * @param a   An ArrayList of type <Station>.
+	 * @param lo  Low index position.
+	 * @param mid Mid index position.
+	 * @param hi  High index position.
+	 */
+	private static void mergeByName(ArrayList<Station> a, int lo, int mid, int hi) {
+		int i = lo, j = mid + 1;
+
+		for (int k = lo; k <= hi; k++)
+			aux2[k] = a.get(k);
+		for (int k = lo; k <= hi; k++)
+			if (i > mid)
+				a.set(k, aux2[j++]);
+			else if (j > hi)
+				a.set(k, aux2[i++]);
+			else if ((aux2[j].getName()).compareTo(aux2[i].getName()) < 0)
+				a.set(k, aux2[j++]);
+			else
+				a.set(k, aux2[i++]);
+	}
+
+	/**
+	 * Function to sort an ArrayList of Stations based on name. Returns a duplicate
+	 * of the original input ArrayList.
+	 * 
+	 * @param a The original ArrayList to sort.
+	 * @return Returns a duplicate ArrayList sorted by name.
+	 */
+	public static ArrayList<Station> sortByName(ArrayList<Station> a) {
+		ArrayList<Station> temp = new ArrayList<Station>();
+		for (int i = 0; i < a.size(); i++) {
+			temp.add(a.get(i));
+		}
+		int n = a.size();
+		aux2 = new Station[n];
+		for (int len = 1; len < n; len *= 2)
+			for (int lo = 0; lo < n - len; lo += len + len)
+				mergeByName(temp, lo, lo + len - 1, Math.min(lo + len + len - 1, n - 1));
+		return temp;
 	}
 
 }
