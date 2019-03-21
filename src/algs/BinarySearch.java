553c9e9198277216f157e7a320e6fe9cc7a390a0
matthewbraden <bradenm@mcmaster.ca>
2019-03-20 22:13:24 -0400

    documentation complete

--- src/algs/BinarySearch.java
+++ src/algs/BinarySearch.java
@@ -15,35 +15,46 @@ public class BinarySearch {
 	 * 
 	 * @param a   An arraylist of type Station to be searched.
 	 * @param key An integer code to search for
-	 * @return
+	 * @return the mid value in the ArrayList
 	 */
 	public static int stationSearch(ArrayList<Station> a, Integer code) {
 		int lo = 0;
 		int hi = a.size() - 1;
-		while (lo <= hi) {
-			int mid = lo + (hi - lo) / 2;
+		while (lo <= hi) { // Searching the arraylist
+			int mid = lo + (hi - lo) / 2; // Changes the mid value
 			if (code < a.get(mid).getCode())
-				hi = mid - 1;
+				hi = mid - 1; // changes the high value
 			else if (code > a.get(mid).getCode())
-				lo = mid + 1;
+				lo = mid + 1; // changes the low value
 			else
-				return mid;
+				return mid; // returns the mid value
 		}
-		return -1;
+		return -1; // if not in the list return -1
 	}
-	
+
+	/**
+	 * 
+	 * Implementation of binary search using code from Sedgewick & Wayne's
+	 * "Algorithms: Fourth Edition" on page 9. Minor modifications have been made to
+	 * implement searching on Stations based on an integer, in order to match the
+	 * API.
+	 * 
+	 * @param a    An arraylist of type Station to be searched.
+	 * @param code Station code in the arraylist
+	 * @return the mid value in the arraylist
+	 */
 	public static int pathSearch(ArrayList<Path> a, int code) {
 		int lo = 0;
 		int hi = a.size() - 1;
-		while (lo <= hi) {
-			int mid = lo + (hi - lo) / 2;
+		while (lo <= hi) { // Searching the arraylist
+			int mid = lo + (hi - lo) / 2; // Changes the mid value
 			if (code < a.get(mid).getEndCode())
-				hi = mid - 1;
+				hi = mid - 1; // changes the high value
 			else if (code > a.get(mid).getEndCode())
-				lo = mid + 1;
+				lo = mid + 1; // changes the low value
 			else
-				return mid;
+				return mid; // returns the mid value
 		}
-		return -1;
+		return -1; // if not in the list return -1
 	}
 }
