4b80d21d9d1191dbb676d7ab2ea6cd38a91c091f
matthewbraden <bradenm@mcmaster.ca>
2019-03-21 23:41:38 -0400

    PastTrips test complete

--- src/tests/PastTripsTest.java
+++ src/tests/PastTripsTest.java
@@ -13,23 +13,36 @@ public class PastTripsTest {
 
 	PastTrips pastTrips = new PastTrips();
 	PastTrips newTrips2 = new PastTrips();
+	PastTrips pastTrips2 = new PastTrips();
 	
 	@Before
 	public void setUp() throws Exception {
 		pastTrips = new PastTrips();
+		pastTrips2 = new PastTrips();
 		PastTrip test1 = new PastTrip(500, 1000, 180);
 		PastTrip test2 = new PastTrip(673, 539, 300);
 		PastTrip test3 = new PastTrip(5743, 599, 730);
 		pastTrips.addTrip(test1);
 		pastTrips.addTrip(test2);
 		pastTrips.addTrip(test3);
+		pastTrips2.addTrip(test2);
+		pastTrips2.addTrip(test1);
+		pastTrips2.addTrip(test3);
 	}
 
 	@Test
-	public void testGetNextPathAndAddTrip() {
+	public void testGetNextPath() {
 		ArrayList<PastTrip> newTrip = new ArrayList<PastTrip>();
 		PastTrip test1 = new PastTrip(500, 1000, 180);
 		newTrip.add(test1);
 		assert pastTrips.getNextPath() == newTrip;
+	}
+	
+	@Test
+	public void testGetNextPath2() {
+		ArrayList<PastTrip> newTrip2 = new ArrayList<PastTrip>();
+		PastTrip test2 = new PastTrip(673, 539, 300);
+		newTrip2.add(test2);
+		assert pastTrips2.getNextPath() == newTrip2;
 	}
 }
