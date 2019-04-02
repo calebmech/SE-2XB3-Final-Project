285ae81fd8e2e122a6c5df9381133f67d4caa9d4
Caleb Mech <cmech1@gmail.com>
2019-04-01 21:41:04 -0400

    Documented IndexMinPQ - Closes #42

--- src/algs/IndexMinPQ.java
+++ src/algs/IndexMinPQ.java
@@ -1,8 +1,17 @@
+// Taken from https://algs4.cs.princeton.edu/code/
+
 package algs;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * Minimum oriented indexed priority queue
+ * 
+ * @author Caleb Mech
+ *
+ * @param <Key> Type of key
+ */
 public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer> {
 	private int maxN;
 	private int n;
@@ -10,6 +19,12 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
 	private int[] qp;
 	private Key[] keys;
 
+	/**
+	 * Constructor for priority queue (PQ)
+	 * 
+	 * @param maxN Max index value
+	 * @throws IllegalArgumentException Thrown if max is less than 0
+	 */
 	public IndexMinPQ(int maxN) {
 		if (maxN < 0)
 			throw new IllegalArgumentException();
@@ -22,20 +37,44 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
 			qp[i] = -1;
 	}
 
+	/**
+	 * Check if PQ is empty
+	 * 
+	 * @return Whether PQ is empty
+	 */
 	public boolean isEmpty() {
 		return n == 0;
 	}
 
+	/**
+	 * Check if PQ contains value at given index
+	 * 
+	 * @param i Index to look at
+	 * @return Whether PQ contains value at index
+	 */
 	public boolean contains(int i) {
 		if (i < 0 || i >= maxN)
 			throw new IllegalArgumentException();
 		return qp[i] != -1;
 	}
 
+	/**
+	 * Size of PQ
+	 * 
+	 * @return Number of keys in PQ
+	 */
 	public int size() {
 		return n;
 	}
 
+	/**
+	 * Insert key into PQ at given index
+	 * 
+	 * @param i Index to insert at
+	 * @param key Key to insert
+	 * @throws IllegalArgumentException Thrown if index is invalid or there is
+	 * already a value at i
+	 */
 	public void insert(int i, Key key) {
 		if (i < 0 || i >= maxN)
 			throw new IllegalArgumentException();
@@ -48,18 +87,36 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
 		swim(n);
 	}
 
+	/**
+	 * Value at index of minimum key
+	 * 
+	 * @return Index of smallest key
+	 * @throws NoSuchElementException Thrown if PQ is empty
+	 */
 	public int minIndex() {
 		if (n == 0)
 			throw new NoSuchElementException("Priority queue underflow");
 		return pq[1];
 	}
 
+	/**
+	 * Value of minimum key
+	 * 
+	 * @return Value of minimum key
+	 * @throws NoSuchElementException Thrown if PQ is empty
+	 */
 	public Key minKey() {
 		if (n == 0)
 			throw new NoSuchElementException("Priority queue underflow");
 		return keys[pq[1]];
 	}
 
+	/**
+	 * Delete minimum element
+	 * 
+	 * @return Index of deleted element
+	 * @throws NoSuchElementException Thrown if PQ is empty
+	 */
 	public int delMin() {
 		if (n == 0)
 			throw new NoSuchElementException("Priority queue underflow");
@@ -73,6 +130,15 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
 		return min;
 	}
 
+	/**
+	 * Key found at index
+	 * 
+	 * @param i Index to look at
+	 * @return Key at given index
+	 * @throws IllegalArgumentException Thrown if index is invalid
+	 * @throws NoSuchElementException Thrown if there is no key
+	 * at the given index
+	 */
 	public Key keyOf(int i) {
 		if (i < 0 || i >= maxN)
 			throw new IllegalArgumentException();
@@ -82,6 +148,15 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
 			return keys[i];
 	}
 
+	/**
+	 * Change key at a given index
+	 * 
+	 * @param i Index to change at
+	 * @param key Key to change to
+	 * @throws IllegalArgumentException Thrown if index is invalid
+	 * @throws NoSuchElementException Thrown if there is no key
+	 * at the given index
+	 */
 	public void changeKey(int i, Key key) {
 		if (i < 0 || i >= maxN)
 			throw new IllegalArgumentException();
@@ -92,6 +167,16 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
 		sink(qp[i]);
 	}
 
+	/**
+	 * Decrease key at a given index
+	 * 
+	 * @param i Index to change at
+	 * @param key Key to change to
+	 * @throws IllegalArgumentException Thrown if index is invalid
+	 * @throws NoSuchElementException Thrown if there is no key
+	 * at the given index
+	 * @throws IllegalArgumentException Thrown if the key would not be decreased
+	 */
 	public void decreaseKey(int i, Key key) {
 		if (i < 0 || i >= maxN)
 			throw new IllegalArgumentException();
@@ -104,6 +189,16 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
 		swim(qp[i]);
 	}
 
+	/**
+	 * Increase key at a given index
+	 * 
+	 * @param i Index to change at
+	 * @param key Key to change to
+	 * @throws IllegalArgumentException Thrown if index is invalid
+	 * @throws NoSuchElementException Thrown if there is no key
+	 * at the given index
+	 * @throws IllegalArgumentException Thrown if the key would not be increased
+	 */
 	public void increaseKey(int i, Key key) {
 		if (i < 0 || i >= maxN)
 			throw new IllegalArgumentException();
@@ -116,6 +211,14 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
 		sink(qp[i]);
 	}
 
+	/**
+	 * Delete key at a given index
+	 * 
+	 * @param i Index to delete at
+	 * @throws IllegalArgumentException Thrown if index is invalid
+	 * @throws NoSuchElementException Thrown if there is no key
+	 * at the given index
+	 */
 	public void delete(int i) {
 		if (i < 0 || i >= maxN)
 			throw new IllegalArgumentException();
@@ -129,10 +232,23 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
 		qp[i] = -1;
 	}
 
+	/**
+	 * Check if key at i is greater than key at j
+	 * 
+	 * @param i Index to compare with
+	 * @param j Index to compare to
+	 * @return Whether key at i is greater than key at j
+	 */
 	private boolean greater(int i, int j) {
 		return keys[pq[i]].compareTo(keys[pq[j]]) > 0;
 	}
 
+	/**
+	 * Exchange keys of i and j
+	 * 
+	 * @param i First index
+	 * @param j Second index
+	 */
 	private void exch(int i, int j) {
 		int swap = pq[i];
 		pq[i] = pq[j];
@@ -141,6 +257,11 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
 		qp[pq[j]] = j;
 	}
 
+	/**
+	 * Swim up key at k
+	 * 
+	 * @param k Index to swim up at
+	 */
 	private void swim(int k) {
 		while (k > 1 && greater(k / 2, k)) {
 			exch(k, k / 2);
@@ -148,6 +269,11 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
 		}
 	}
 
+	/**
+	 * Sink down key at k
+	 * 
+	 * @param k Index to swim down at
+	 */
 	private void sink(int k) {
 		while (2 * k <= n) {
 			int j = 2 * k;
@@ -160,6 +286,9 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
 		}
 	}
 
+	/**
+	 * Iterator that iterates over keys in PQ
+	 */
 	public Iterator<Integer> iterator() {
 		return new HeapIterator();
 	}
